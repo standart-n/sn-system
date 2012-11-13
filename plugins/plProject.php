5c7cc39c15c9cf57bdf0ec40db272b19d93d7a37
aleksnick <aleksnick@list.ru>
2012-11-13 10:19:11 +0400

    сделал чтобы можно было при желании напрявлять обычные и аякс запросы на один и тотже файлы - start.php. удалил лишние плагины

--- plugins/plProject.php
+++ plugins/plProject.php
@@ -1,16 +1,16 @@
 <?php class plProject extends sn {
 
 function __construct() {
-	if (!isAjax()) {
-		load("index.html");
+	if ((isAjax()) && (file_exists(project."/controls/ajax.php"))) {
+		require_once(project."/controls/ajax.php");
+		sn::cl("ajax");
+	} else {
+		if (file_exists(project."/index.html")) {
+			load("index.html");
+		}
 		if (file_exists(project."/controls/start.php")) {
 			require_once(project."/controls/start.php");
 			sn::cl("start");
-		}
-	} else {
-		if (file_exists(project."/controls/ajax.php")) {
-			require_once(project."/controls/ajax.php");
-			sn::cl("ajax");
 		}
 	}
 }
