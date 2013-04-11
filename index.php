24547c871b6be1312a5b6fa02b2f8eda67151cbb
aleksnick <aleksnick@list.ru>
2013-04-11 16:28:14 +0400

    автоматическое опеределение пути, по которому находится проект

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <?php
-	const system="/var/web/st-n/_system/sn-project/sn-system";
-	const project="/var/web/st-n/_system/sn-project";
+	define("project",preg_replace('/\/([a-zA-Z0-9\.\-\!\~])+$/','/',$_SERVER['SCRIPT_FILENAME']));
+	define("system",project."sn-system");
+	define("core",system.'/core/sn.php');
 	
-	if (file_exists(system.'/core/sn.php')) {
-		require_once(system.'/core/sn.php');
+	if (file_exists(core)) {
+		require_once(core);
 		$sn=new sn;
+	} else {
+		echo "not found ".core;
 	}
 ?>
--- plugins/plProject.php
+++ plugins/plProject.php
@@ -5,9 +5,6 @@ function __construct() {
 		require_once(project."/controls/ajax.php");
 		sn::cl("ajax");
 	} else {
-		if (file_exists(project."/index.html")) {
-			load("index.html");
-		}
 		if (file_exists(project."/controls/start.php")) {
 			require_once(project."/controls/start.php");
 			sn::cl("start");
